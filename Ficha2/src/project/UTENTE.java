670ba0b9e35286d4ae39dc1cdc1e56b5b662c8dc
jamqu <jamqu@10.180.4.106>
2022-10-03 14:35:50 +0100

    Teste

---
+++ Ficha2/src/project/UTENTE.java
@@ -0,0 +1,21 @@
+package project;
+
+public class UTENTE {
+
+	
+	public class Utente {	
+		private String nome;	 
+		private int idade;	
+		private float altura;	
+		private int peso;	
+		
+		
+		public Utente(String nome, int idade, float altura, int peso){
+			this.nome = nome;
+			this.idade = idade;
+			this.altura = altura;
+			this.peso = peso;
+			
+		}
+	}
+}
