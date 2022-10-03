2eb67fab6f1d7a8d624cccf2cf855547df43a2a4
jamqu <jamqu@10.180.4.106>
2022-10-03 14:47:31 +0100

    getandSet

--- Ficha2/src/project/UTENTE.java
+++ Ficha2/src/project/UTENTE.java
@@ -17,5 +17,70 @@ public class UTENTE {
 			this.peso = peso;
 			
 		}
+
+
+		/**
+		 * @return the nome
+		 */
+		public String getNome() {
+			return nome;
+		}
+
+
+		/**
+		 * @param nome the nome to set
+		 */
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+
+		/**
+		 * @return the idade
+		 */
+		public int getIdade() {
+			return idade;
+		}
+
+
+		/**
+		 * @param idade the idade to set
+		 */
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+
+
+		/**
+		 * @return the altura
+		 */
+		public float getAltura() {
+			return altura;
+		}
+
+
+		/**
+		 * @param altura the altura to set
+		 */
+		public void setAltura(float altura) {
+			this.altura = altura;
+		}
+
+
+		/**
+		 * @return the peso
+		 */
+		public int getPeso() {
+			return peso;
+		}
+
+
+		/**
+		 * @param peso the peso to set
+		 */
+		public void setPeso(int peso) {
+			this.peso = peso;
+		}
 	}
+	
 }
